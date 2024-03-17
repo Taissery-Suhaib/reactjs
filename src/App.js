35c53361bbc484534e387989daead522808b5549
taissery <taisserysuhaib@gmail.com>
2024-03-17 14:58:09 +0530

    check push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Newcomponent from "../src/new_component/newcomponent"
+function App() {
+  return (
+<>
+<h1> hello how are hello </h1>
+<Newcomponent/>
+</>
+  );
+}
+
+export default App;
---
+++ src/new_component/newcomponent.js
@@ -0,0 +1,9 @@
+function Newcomponent() {
+    return (
+  <>
+  <h1> hello check  </h1>
+  </>
+    );
+  }
+  
+  export default Newcomponent;
